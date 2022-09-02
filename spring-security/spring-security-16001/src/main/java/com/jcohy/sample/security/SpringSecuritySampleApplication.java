ba099f156556928e381f12d38853a1839b3ae3f6
jiac <jcohy1203>
2022-09-02 19:08:08 +0800

    add spring-security

---
+++ spring-security/spring-security-16001/src/main/java/com/jcohy/sample/security/SpringSecuritySampleApplication.java
@@ -0,0 +1,22 @@
+package com.jcohy.sample.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述: .
+ * <p>
+ * Copyright © 2022 <a href="https://www.jcohy.com" target= "_blank">https://www.jcohy.com</a>
+ * </p>
+ *
+ * @author jiac
+ * @version 2022.04.0 2022/9/2:18:37
+ * @since 2022.04.0
+ */
+@SpringBootApplication
+public class SpringSecuritySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuritySampleApplication.class,args);
+    }
+}
---
+++ spring-security/spring-security-16001/src/main/java/com/jcohy/sample/security/web/IndexController.java
@@ -0,0 +1,45 @@
+package com.jcohy.sample.security.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 描述: .
+ * <p>
+ * Copyright © 2022 <a href="https://www.jcohy.com" target= "_blank">https://www.jcohy.com</a>
+ * </p>
+ *
+ * @author jiac
+ * @version 2022.04.0 2022/9/2:18:38
+ * @since 2022.04.0
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/index";
+    }
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/user/index")
+    public String userIndex() {
+        return "user/index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/login-error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError",true);
+        return "login";
+    }
+}
