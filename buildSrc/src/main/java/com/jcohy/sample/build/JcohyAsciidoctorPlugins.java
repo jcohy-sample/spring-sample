99785bf1340ce0311f57018c81a6670e8b7da285
jiac <jia_chao23@126.com>
2022-12-05 16:18:48 +0800

    add action

--- buildSrc/src/main/java/com/jcohy/sample/build/JcohyAsciidoctorPlugins.java
+++ buildSrc/src/main/java/com/jcohy/sample/build/JcohyAsciidoctorPlugins.java
@@ -48,5 +48,10 @@ public class JcohyAsciidoctorPlugins implements Plugin<Project> {
 
     private void addAsciidoctorTaskAttributes(Project project,Map<String, Object> attributes) {
         attributes.put("rootProject", project.getRootProject().getProjectDir());
+        attributes.put("java-code", project.getRootProject().getProjectDir() + "/src/main/java/com/jcohy/sample");
+        attributes.put("kotlin-code", project.getRootProject().getProjectDir() + "/src/main/kotlin/com/jcohy/sample");
+        attributes.put("go-code", project.getRootProject().getProjectDir() + "/src/main/go/com/jcohy/sample");
+        attributes.put("oss-images", "https://study.jcohy.com/images/spring");
+        attributes.put("spring-boot-chinese-docs","http://docs.jcohy.com/docs/spring-boot/2.7.1/html5/zh-cn");
     }
 }
