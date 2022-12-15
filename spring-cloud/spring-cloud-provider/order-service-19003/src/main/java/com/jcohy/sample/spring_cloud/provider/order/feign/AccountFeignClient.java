16b9952276955509f5e2d65dc089ac7b8bb41468
jiac <jia_chao23@126.com>
2022-12-15 16:09:56 +0800

    add nacos + seata

--- spring-cloud/spring-cloud-provider/order-service-19003/src/main/java/com/jcohy/sample/spring_cloud/provider/order/feign/AccountFeignClient.java
+++ spring-cloud/spring-cloud-provider/order-service-19003/src/main/java/com/jcohy/sample/spring_cloud/provider/order/feign/AccountFeignClient.java
@@ -12,7 +12,7 @@ import java.math.BigDecimal;
  * @author fangliangsheng
  * @date 2019-04-04
  */
-@FeignClient(name = "account-service", url = "127.0.0.1:8083")
+@FeignClient(name = "account-service", url = "127.0.0.1:19001")
 public interface AccountFeignClient {
 
 	@GetMapping("/debit")
